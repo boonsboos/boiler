08f0be0fdbbfeee0194e02886c99a6a3fe7a1966
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2022-01-01 22:07:47 +0100

    more work on nal parser

--- nal/compiler/parser.v
+++ nal/compiler/parser.v
@@ -10,6 +10,39 @@ pub fn parse(file_name string) {
 		exit(1)
 	}
 
+	file_contents := os.read_lines(file_name) or { panic('could not open file! $err') }
 
+	mut words := []string{}
+	mut line_counter := 0
+	for line in file_contents {
+		line_counter++
+		mut sentence := line.trim_space()
+		mut char_counter := 0
+		for char in sentence {
+			char_counter++
+			match char {
+				0x2c { // comma
+					words << sentence.all_before(',')
+					sentence = sentence.substr(sentence.index(',') or { panic('could not find index') }+1, sentence.len)
+				}
+				0x20 { // space
+					words << sentence.all_before(' ')
+					sentence = sentence.substr(sentence.index(' ') or { panic('could not find index') }+1, sentence.len)
+				}
+				0x5b {
+					if sentence.index('[') or {0} +1 == 0x5d {
+						println('array definition found')
+					} else {
+						throw_compiler_error('found opening square bracket but no closing square bracket...', .error, line_counter, char_counter, file_name)
+					}
+					words << sentence.all_before(' ')
+					sentence = sentence.substr(sentence.index(']') or { panic('could not find index') }+1, sentence.len)
+				}
+
+				else { continue }
+			}
+		}
+	}
+	println(words)
 
 }
